8fe43be1b3f49bae6658a7457eeb3265d41cdf85
Craig Reeder <creeder@uturndata.com>
2021-09-09 10:42:34 -0500

    Initial presentation setup

---
+++ present.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -z $1 ]; then
+  echo "What is it called? (pass an argument)"
+  exit
+fi
+
+mkdir $1
+cd $1
+npx degit tmcw/big
+git init
+git add .
+git commit -m "Initial commit"
